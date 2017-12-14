04deeac306ea89f8f13acdecb7c2235100b71b31
MLSchreur <mschreur@zeelandnet.nl>
2017-12-14 21:38:58 +0100

    added ngmodel to input section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  powerSupply   : number;
+  ledPowerDrop  : number;
+  ledCurrent    : number;
+  numberOfLeds  : number;
+
+
+  calculate(){
+    console.log(this.powerSupply);
+    console.log(this.ledPowerDrop);
+    console.log(this.ledCurrent);
+    console.log(this.numberOfLeds);
+  }
+
 }
